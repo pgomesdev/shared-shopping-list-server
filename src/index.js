fd047125a5ebd14c3e501b1ae0819f29937d5fcb
pgomesdev <gomes.pedro15@gmail.com>
2019-02-06 20:07:33 -0300

    feat: added a new query operation and mutation operations
    get operation: shoppingList - returns a shopping list by its id
    mutation operations: createShoppingList - create a new shopping list
    addStoreToShoppingList - add a store to a shopping list
    addItemToShoppingList - add a item to a shopping list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
 
 let shoppingList = [
   {
     name: 'Mercado',
     items: [0, 1],
+    store: ['Walmart', 'Extra'],
   },
   {
     name: 'PC Gamer',
     items: [2, 3],
+    store: ['Walmart', 'Extra'],
   }
-]
+];
 
 let items = [
   {
@@ -32,23 +34,24 @@ let items = [
     price: null,
     quantity: 1,
   }
-]
+];
 
 const resolvers = {
   Query: {
     info: () => `This is the API of Shared Shopping List`,
     listShoppingList: () => shoppingList,
+    shoppingList
   },
   ShoppingList: {
     items: (parent) => {
       return parent.items.map(item => items[item])
     },
   }
-}
+};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-})
+});
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log(`Server is running on http://localhost:4000`));
