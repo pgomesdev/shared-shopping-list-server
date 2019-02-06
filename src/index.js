43c6d3fb6053f7f7b25698c0aacd313efb74d194
pgomesdev <gomes.pedro15@gmail.com>
2019-02-05 21:03:58 -0300

    feat: added query operation to the API
    now the API returns a full list of shopping lists with their items info

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let shoppingList = [
+  {
+    name: 'Mercado',
+    items: [0, 1],
+  },
+  {
+    name: 'PC Gamer',
+    items: [2, 3],
+  }
+]
+
+let items = [
+  {
+    name: 'Arroz',
+    price: null,
+    quantity: null,
+  },
+  {
+    name: 'Monster',
+    price: 6.99,
+    quantity: 10,
+  },
+  {
+    name: 'Mouse',
+    price: null,
+    quantity: null,
+  },
+  {
+    name: 'Teclado',
+    price: null,
+    quantity: 1,
+  }
+]
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of Shared Shopping List`,
+    listShoppingList: () => shoppingList,
+  },
+  ShoppingList: {
+    items: (parent) => {
+      return parent.items.map(item => items[item])
+    },
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
